465ec631f6092eda0c8ba364b57bd2d7d5984df4
itskerrin <itskerrin@gmail.com>
2025-06-03 14:21:57 +0100

    Removed unused code

--- app/api/getUsers/route.ts
+++
@@ -1,8 +0,0 @@
-// import type { Database } from '@/types/database.types'
-// import { createClient } from '@supabase/supabase-js'
-
-// const supabaseUrl = 'https://kgaiyzetfkfggzlezsll.supabase.co'
-// const supabaseKey = process.env.SUPABASE_ACCESS_TOKEN
-
-// if (!supabaseKey) throw new Error('Missing access token.');
-// const supabase = createClient(supabaseUrl, supabaseKey)
