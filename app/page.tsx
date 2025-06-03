187dd181f5736de279687402416b5b277fa30ae3
itskerrin <itskerrin@gmail.com>
2025-06-03 15:12:44 +0100

    Started to build out event and profile page structure.

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <div className="">
-      Gig buddy app
+    <div>
+      <div>
+        <h1>Gig buddy app</h1>
+      </div>
+      <div className="flex flex-col items-center">
+        <Link href="/account">My account</Link>
+        <Link href="/event/1">Event 1</Link>
+        <Link href="/profile/12345">Profile</Link>
+      </div>
     </div>
   );
 }
